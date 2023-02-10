62b487db84974d5b82e7789178dd1740fb3cfc52
Jangwook Ryu <rxjw95@gmail.com>
2023-02-11 00:41:05 +0900

    refactor(Money Value record로 변경)

--- src/main/java/com/toys/lottogame/purchase/domain/Money.java
+++ src/main/java/com/toys/lottogame/purchase/domain/Money.java
@@ -1,9 +1,4 @@
 package com.toys.lottogame.purchase.domain;
 
-public class Money {
-    private final int value;
-
-    public Money(int value) {
-        this.value = value;
-    }
+public record Money(int value) {
 }
