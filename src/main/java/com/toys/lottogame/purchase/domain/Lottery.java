cb4f0ba164a17876d0c9808fce64eb7c7ba3abfc
Jangwook Ryu <rxjw95@gmail.com>
2023-02-11 01:17:18 +0900

    feature(로또 도메인 룰 추가)
    - 로또의 게임은 1개보다 크거나 같고 5개보다 작거나 같다.

--- src/main/java/com/toys/lottogame/purchase/domain/Lottery.java
+++ src/main/java/com/toys/lottogame/purchase/domain/Lottery.java
@@ -1,12 +1,21 @@
 package com.toys.lottogame.purchase.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Lottery {
     private final List<Game> games;
 
     public Lottery(List<Game> games) {
+        Objects.requireNonNull(games, "games is null.");
+        verifyGamesMoreThanOrEqualToOneAndLessThanOrEqualToFive(games);
         this.games = games;
+    }
+
+    private void verifyGamesMoreThanOrEqualToOneAndLessThanOrEqualToFive(List<Game> games) {
+        if (1 > games.size() || 5 < games.size()) {
+            throw new IllegalArgumentException("games size must be 1 <= game <= 5 in lottery.");
+        }
     }
 
     public int getGameCount() {
