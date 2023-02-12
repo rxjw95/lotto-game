35730d45ed511ed222466052d92deaea851c884c
Jangwook Ryu <rxjw95@gmail.com>
2023-02-13 03:13:21 +0900

    test(구매 도메인 테스트 코드)
    https://github.com/rxjw95/lotto-game/issues/3 이슈의 구매 도메인 테스트 코드 추가

--- src/test/java/com/toys/lottogame/purchase/domain/PurchaseTest.java
+++ src/test/java/com/toys/lottogame/purchase/domain/PurchaseTest.java
@@ -1,19 +1,21 @@
 package com.toys.lottogame.purchase.domain;
 
-import org.junit.jupiter.api.Test;
+import com.toys.lottogame.purchase.domain.vo.Games;
+import com.toys.lottogame.purchase.domain.vo.Money;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class PurchaseTest {
-
-    @Test
-    public void 구매자_정보가_있어야_한다() {
-
-    }
+class PurchaseTest extends PurchaseDomainDummy {
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5})
-    public void 구매가격은_로또의_게임의_수_곱하기_게임_가격이다(int size) {
+    public void 구매가격은_로또의_게임의_수_곱하기_게임_가격이다(int count) {
+        Games gameDummies = makeGamesDummy(count);
+        Purchaser purchaserDummy = new Purchaser("wafo");
+
+        Purchase purchase = new Purchase(gameDummies, purchaserDummy);
+        Assertions.assertThat(purchase.getAmount()).isEqualTo(new Money(count * 1_000));
     }
 
 }
